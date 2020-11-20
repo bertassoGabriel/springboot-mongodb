e7a27ab0da25a29fe8f0ec4df808947c5f97f830
bertassoGabriel <gabrieljacintho868@hotmail.com>
2020-11-20 05:31:32 -0400

    Primeiro commit - Projeto criado

---
+++ src/test/java/com/bertasso/webservices/MongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bertasso.webservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
