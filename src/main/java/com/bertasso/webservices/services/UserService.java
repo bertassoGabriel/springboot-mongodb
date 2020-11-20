2160a8181d1f1e56d74148d2d242cdde25c9a282
bertassoGabriel <gabrieljacintho868@hotmail.com>
2020-11-20 13:11:52 -0400

    Atualização de usuário com PUT

--- src/main/java/com/bertasso/webservices/services/UserService.java
+++ src/main/java/com/bertasso/webservices/services/UserService.java
@@ -1,5 +1,6 @@
 package com.bertasso.webservices.services;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -35,6 +36,17 @@ public class UserService {
 		repo.deleteById(id);
 	}
 	
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
