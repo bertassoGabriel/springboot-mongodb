c6683cfcb623abcab52c3189fdb63df15a446348
bertassoGabriel <gabrieljacintho868@hotmail.com>
2020-11-20 09:56:52 -0400

    Operação de instanciação da base de dados

---
+++ src/main/java/com/bertasso/webservices/config/Instantiation.java
@@ -0,0 +1,29 @@
+package com.bertasso.webservices.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.bertasso.webservices.domain.User;
+import com.bertasso.webservices.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+	}
+
+}
