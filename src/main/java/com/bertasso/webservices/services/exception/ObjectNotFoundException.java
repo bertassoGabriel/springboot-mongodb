3724a279be67756bd215e18b343d9fb3d2fefe34
bertassoGabriel <gabrieljacintho868@hotmail.com>
2020-11-20 11:31:09 -0400

    Obtendo um usuário por id

---
+++ src/main/java/com/bertasso/webservices/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,10 @@
+package com.bertasso.webservices.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+
+}
